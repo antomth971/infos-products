5dda6b902ae289c9aba5d25bf27a05666e9dc5e2
Anthony Mathieu <anthonymathieu@MacBook-Air-de-Anthony.local>
2025-12-29 18:34:49 +0100

    hebergeur contabo*

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const puppeteer = require('puppeteer');
 const session = require('express-session');
 const Product = require('./models/Product');
 const IgnoredProduct = require('./models/IgnoredProduct');
-
+const NODE_ENV = process.env.NODE_ENV || 'development';
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/web-scraper';
